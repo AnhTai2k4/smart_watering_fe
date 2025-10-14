74ef1da071245bd03d9dc1c789f9ef5cee7ae80a
mrBunachi <hoangtsugumi194@gmail.com>
2025-10-14 17:32:05 +0700

    Header LordCark

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
-const Header = () => {
+import "./Header.css"
+
+function Header() {
   return (
-    <div>
-        Header
-    </div>
+    <header className="header">
+      <div className="logo_group">
+        <img src="/Tree.png" alt="Tree" className="picture" />
+        <h1 className="logo">Smart Sprout</h1>
+      </div>
+
+      <nav className="notiandvery">
+        <img src="/Bell.png" />
+        <img src="/User.png" />
+      </nav>
+    </header>
   )
 }
 
