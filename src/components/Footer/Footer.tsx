18ad80a01720505e6148c6eee351bf551ad6ff7a
AnhTai2k4 <tranthanhanhtai2909@gmail.com>
2025-10-18 19:53:33 +0700

    xay device_context

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,27 @@
-const Header = () => {
+import "./Footer.css"
+
+function Footer() {
   return (
-    <div>
-        Footer
-    </div>
+    <footer className="footer">
+      <div className="footer__left">
+        <h2 className="footer__logo">Smart Watering</h2>
+        <p className="footer__desc">
+          Giải pháp tưới cây thông minh giúp tiết kiệm nước và công sức.
+        </p>
+      </div>
+
+      <div className="footer__links">
+        <a href="#">Giới thiệu</a>
+        <a href="#">Hỗ trợ</a>
+        <a href="#">Chính sách bảo hành</a>
+        <a href="#">Liên hệ</a>
+      </div>
+
+      <div className="footer__right">
+        <p>© 2025 Smart Watering. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
-export default Header
+export default Footer
